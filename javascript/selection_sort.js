8dae0c187f80f1eec8142f31c872c793a9cae880
jrrohrer <jessie.rohrer@gmail.com>
2021-11-19 11:33:16 -0500

    commit tree 342f5efb474ec032c1707ee653d11e68c9ca9b37

--- javascript/selection_sort.js
+++ javascript/selection_sort.js
@@ -1,5 +1,12 @@
 function selectionSort(arr) {
-  // type your code here
+	const sortedArr = []
+	while (arr.length > 0) {
+		const min = Math.min(...arr)
+		const index = arr.indexOf(min)
+		sortedArr.push(min)
+		arr.splice(index, 1)
+	}
+	return sortedArr
 }
 
 if (require.main === module) {
@@ -7,17 +14,34 @@ if (require.main === module) {
   console.log("Expecting: [-1, 2, 3, 5]");
   console.log("=>", selectionSort([3, -1, 5, 2]));
 
-  console.log("");
-
   // BENCHMARK HERE, and print the average runtime
   const longInput = [];
 
   for (let i = 0; i < 100; ++i) {
     longInput.push(Math.random());
   }
+
+  const startTime = Date.now();
+
+  for (let i = 0; i < 1000; ++i) {
+    selectionSort([2, 1]);
+    selectionSort(longInput);
+  }
+
+  const avgTime = (Date.now() - startTime) / 2000;
+
+  console.log(avgTime);
 }
 
 module.exports = selectionSort;
 
 // Please add your pseudocode to this file
+  /**************************************
+   * declare an empty array called sortedArr
+   * loop over arr while arr has elements
+   *  find minimum value
+   *  push it to sortedArr
+   *  splice it from the original arr
+   * return sortedArr
+   */
 // And a written explanation of your solution
--- ruby/selection_sort.rb
+++ ruby/selection_sort.rb
@@ -1,5 +1,12 @@
 def selection_sort(arr)
-  # type your code in here
+  sorted_arr = []
+  while arr.length > 0
+    min = arr.min
+    index = arr.index(min)
+    sorted_arr.push(min)
+    arr.delete_at(index)
+  end
+  sorted_arr
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -15,6 +22,16 @@ if __FILE__ == $PROGRAM_NAME
   long_input = []
 
   100.times { long_input << rand }
+
+  start_time = Time.now
+
+  1000.times do 
+    selection_sort([2, 1])
+    selection_sort(long_input)
+  end
+
+  avg_time = (Time.now - start_time) / 1000
+  puts avg_time
 end
 
 # Please add your pseudocode to this file
